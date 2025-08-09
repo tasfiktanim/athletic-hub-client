6320f96c00d269360091e46abb5f41a19cdc5333
tasfiktanim <tasfiktanimofficial@gmail.com>
2025-08-10 02:58:32 +0600

    sticky navbar

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,14 +1,17 @@
-
 import { Outlet } from 'react-router';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
 const MainLayout = () => {
   return (
-    <div className="flex flex-col min-h-screen justify-between">
-      <Navbar />
+    <div className="flex flex-col min-h-screen">
+      {/* Navbar fixed at top, full width, above other elements */}
+      <header className="fixed top-0 left-0 w-full z-50">
+        <Navbar />
+      </header>
 
-      <main className="flex-grow container mx-auto px-4 py-6">
+      {/* Add top padding equal to Navbar height so content doesn't hide behind it */}
+      <main className="flex-grow container mx-auto px-4 py-6 pt-20">
         <Outlet />
       </main>
 
